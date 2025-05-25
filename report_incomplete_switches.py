4cb750140fe6a6c273e0c6f3c8cf78c835fc767f
mmarrano <mark.marrano@gmail.com>
2025-05-25 10:43:07 -0500

    change way we log the json

--- report_incomplete_switches.py
+++ report_incomplete_switches.py
@@ -17,18 +17,16 @@ class ListRacks(Script):
             site = Site.objects.get(pk=rack.site.pk)
             self.log_info(f"ID: {rack.pk}, Rack: {rack.name}, Site: {site.name}, Status: {rack.status}")
             
-        # Prepare JSON data
-        racks_data = []
-        for rack in racks:
-            site = Site.objects.get(pk=rack.site.pk)
-            racks_data.append({
+        # Optionally, you can return the data in JSON format
+        racks_data = [
+            {
                 "id": rack.pk,
                 "name": rack.name,
                 "site": rack.site.name,
                 "status": rack.status
-            })
-
-        # Convert to JSON and log it
-        racks_json = json.dumps(racks_data, indent=4)
-        self.log_info("Racks data in JSON format:")
-        self.log_info(racks_json)
+            }
+            for rack in racks
+        ]
+        
+        self.log_info(json.dumps(racks_data, indent=4))
+        return racks_data
