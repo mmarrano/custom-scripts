82a262299c3ad6c4f2350a965d43b3b3273eaa16
mmarrano <mark.marrano@gmail.com>
2025-05-25 11:13:41 -0500

    add new custom script to report geo data

---
+++ report_bad_geo_sites.py
@@ -0,0 +1,26 @@
+# site_address.py
+
+# Make sure to add `geocoder` to your `local_requirements.txt` and make sure it is installed in your Python venv.
+
+import geocoder
+from extras.scripts import Script
+from dcim.models import Site
+
+class checkSiteAddress(Script):
+    class Meta: #type: ignore
+        name = "Report Bad Geo Sites"
+        description = "Checks for sites with missing or inaccurate physical addresses or geo locations."
+    
+    def run(self, data, commit):
+        for site in Site.objects.all():
+            if site.physical_address:
+                if site.latitude and site.longitude:
+                    self.log_success(site)
+                else:
+                    g = geocoder.osm(site.physical_address)
+                    if g:
+                        self.log_warning(site, f'Missing geo location - possible ({round(g.x,6)}, {round(g.y,6)})')
+                    else:
+                        self.log_warning(site, f'Missing geo location ({site.latitude}, {site.longitude})')    
+            else:
+                self.log_failure(site, f'Site name: {site.name}; Missing physical address')
