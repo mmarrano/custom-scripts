de1a026d0c98a332f930836aefcff63e455a4bfd
mmarrano <mark.marrano@gmail.com>
2025-05-25 10:24:37 -0500

    add script to report on available racks

---
+++ report_incomplete_switches.py
@@ -0,0 +1,16 @@
+from extras.scripts import Script
+from dcim.models import Rack
+
+class ListRacks(Script):
+    class Meta: #type: ignore
+        name = "List Racks"
+        description = "Displays a list of registered racks, including their site and status."
+
+    def run(self, data, commit):
+        racks = Rack.objects.all()
+        if not racks:
+            self.log_warning("No racks found.")
+            return
+
+        for rack in racks:
+            self.log_info(f"Rack: {rack.name}, Site: {rack.site.name}, Status: {rack.status}")
